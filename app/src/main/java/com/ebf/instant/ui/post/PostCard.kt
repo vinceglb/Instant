88ee0abfc0cba5544f8f834be0e433bc83af4a75
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2021-10-18 17:13:52 +0200

    💄 Improving PostCard UI

--- app/src/main/java/com/ebf/instant/ui/post/PostCard.kt
+++ app/src/main/java/com/ebf/instant/ui/post/PostCard.kt
@@ -1,5 +1,6 @@
 package com.ebf.instant.ui.post
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -14,6 +15,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.ImagePainter
 import coil.compose.rememberImagePainter
 import com.ebf.instant.R
 import com.ebf.instant.model.Post
@@ -24,15 +26,17 @@ import java.util.*
 @Composable
 fun PostCard(
     post: Post,
-    placeholder: Boolean = false
+    preview: Boolean = false
 ) {
-    val painter = rememberImagePainter(
-        data = post.imageUrl,
-        builder = {
-            crossfade(true)
-            if (placeholder) placeholder(R.drawable.post_image_example)
-        }
-    )
+    @Composable
+    fun painter(url: String, @DrawableRes tool: Int): ImagePainter =
+        rememberImagePainter(
+            data = url,
+            builder = {
+                crossfade(true)
+                if (preview) placeholder(tool)
+            }
+        )
 
     Column {
         Row(
@@ -42,8 +46,9 @@ fun PostCard(
                 .padding(top = 8.dp, bottom = 4.dp)
         ) {
             Image(
-                painter = rememberImagePainter(data = post.user.imageUrl),
+                painter = painter(url = post.user.imageUrl, tool = R.drawable.profile_pitcure),
                 contentDescription = "Image de profile",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(30.dp)
                     .clip(CircleShape)
@@ -62,7 +67,7 @@ fun PostCard(
             Box(contentAlignment = Alignment.Center) {
                 CircularProgressIndicator()
                 Image(
-                    painter = painter,
+                    painter = painter(url = post.imageUrl, tool = R.drawable.post_image_example),
                     contentDescription = "Image du post",
                     contentScale = ContentScale.FillWidth,
                     modifier = Modifier
@@ -82,10 +87,10 @@ fun PostCardPreview() {
     val post = Post(
         id = "test",
         user = User(id = "test", username = "vince.app", name = "Vincent", ""),
-        imageUrl = "url",
+        imageUrl = "",
         date = Date()
     )
     InstantTheme {
-        PostCard(post = post, placeholder = true)
+        PostCard(post = post, preview = true)
     }
 }
