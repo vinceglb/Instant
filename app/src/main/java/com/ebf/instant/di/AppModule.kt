03c299567dc01638d5ddb87fde2ba4a4818d768c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2021-11-03 02:29:57 +0100

    ♻️ Refactor AppModule

--- app/src/main/java/com/ebf/instant/di/AppModule.kt
+++ app/src/main/java/com/ebf/instant/di/AppModule.kt
@@ -1,20 +1,37 @@
 package com.ebf.instant.di
 
-import com.ebf.instant.local.AppDatabase
-import com.ebf.instant.local.DataStorePreferencesStorage
-import com.ebf.instant.local.PreferencesStorage
-import com.ebf.instant.local.dataStore
-import com.ebf.instant.remote.*
-import com.ebf.instant.repo.PostRepository
-import com.ebf.instant.repo.UserRepository
+import com.ebf.instant.data.AuthRepository
+import com.ebf.instant.data.db.AppDatabase
+import com.ebf.instant.data.post.FirestorePostDataSource
+import com.ebf.instant.data.post.FunctionsPostDataSource
+import com.ebf.instant.data.post.PostRepository
+import com.ebf.instant.data.post.StoragePostDataSource
+import com.ebf.instant.data.prefs.DataStorePreferencesStorage
+import com.ebf.instant.data.prefs.PreferencesStorage
+import com.ebf.instant.data.prefs.dataStore
+import com.ebf.instant.data.signin.datasources.AuthStateUserDataSource
+import com.ebf.instant.data.signin.datasources.FirebaseAuthStateUserDataSource
+import com.ebf.instant.data.signin.datasources.FirestoreRegisteredUserDataSource
+import com.ebf.instant.data.signin.datasources.RegisteredUserDataSource
+import com.ebf.instant.data.user.FirestoreUserDataSource
+import com.ebf.instant.data.user.UserRepository
+import com.ebf.instant.domain.auth.ObserveUserAuthStateUseCase
+import com.ebf.instant.fcm.FcmTokenUpdater
+import com.ebf.instant.ui.InstantAppViewModel
 import com.ebf.instant.ui.camera.CameraScreenViewModel
+import com.ebf.instant.ui.create.CreateAccountViewModel
+import com.ebf.instant.ui.feed.FeedViewModel
 import com.ebf.instant.ui.login.LoginScreenViewModel
-import com.ebf.instant.ui.post.FeedScreenViewModel
-import com.ebf.instant.ui.post.comment.CommentScreenViewModel
+import com.ebf.instant.ui.signin.FirebaseSignInViewModelDelegate
+import com.ebf.instant.ui.signin.SignInViewModelDelegate
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -24,15 +41,18 @@ val appModule = module {
     single { Firebase.firestore }
     single { Firebase.auth }
     single { Firebase.storage }
+    single { Firebase.functions("europe-west1") }
 
-    // Data source
-    single<PostDataSource> { FirestorePostDataSource(get()) }
-    single<UserDataSource> { FirestoreUserDataSource(get()) }
-    single { StorageDataSource(get(), get(), androidContext()) }
+    // Data sources
+    single { FirestoreUserDataSource(get()) }
+    single { FirestorePostDataSource(get()) }
+    single { FunctionsPostDataSource(get()) }
+    single { StoragePostDataSource(get(), get()) }
 
     // Repositories
-    single { PostRepository(get(), get(), get(), get(), get(), get(), get(), get()) }
     single { UserRepository(get()) }
+    single { AuthRepository(get()) }
+    single { PostRepository(get(), get(), get(), get(), get(), get(), get(), get()) }
 
     // Room db
     single { AppDatabase.init(androidContext()) }
@@ -45,9 +65,20 @@ val appModule = module {
     factory { androidContext().dataStore }
     single<PreferencesStorage> { DataStorePreferencesStorage(get()) }
 
+    // Scope
+    single { CoroutineScope(SupervisorJob() + Dispatchers.Default) }
+
     // ViewModels
-    viewModel { FeedScreenViewModel(get()) }
-    viewModel { CameraScreenViewModel(get()) }
-    viewModel { LoginScreenViewModel(get(), get(), get()) }
-    viewModel { CommentScreenViewModel(get()) }
+    viewModel { InstantAppViewModel(get()) }
+    viewModel { CreateAccountViewModel(get(), get(), get()) }
+    viewModel { LoginScreenViewModel(get()) }
+    viewModel { FeedViewModel(get(), get(), get()) }
+    viewModel { CameraScreenViewModel(get(), get()) }
+
+    factory<RegisteredUserDataSource> { FirestoreRegisteredUserDataSource(get()) }
+    single<AuthStateUserDataSource> { FirebaseAuthStateUserDataSource(get(), get(), get()) }
+    factory { FcmTokenUpdater(get(), get()) }
+
+    single { ObserveUserAuthStateUseCase(get(), get(), get()) }
+    single<SignInViewModelDelegate> { FirebaseSignInViewModelDelegate(get(), get()) }
 }
