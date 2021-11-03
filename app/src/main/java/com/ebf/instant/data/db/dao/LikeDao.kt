8baef5625960fb2b049c3a9e2fe95444e64afff6
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2021-11-03 02:23:18 +0100

    🚚 Move db classes in data folder

---
+++ app/src/main/java/com/ebf/instant/data/db/dao/LikeDao.kt
@@ -0,0 +1,22 @@
+package com.ebf.instant.data.db.dao
+
+import androidx.room.*
+import com.ebf.instant.model.Like
+import com.ebf.instant.model.User
+
+@Dao
+interface LikeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user: User, like: Like)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertList(likes: List<Like>)
+
+    @Query("SELECT * FROM `Like` WHERE postId = :postId AND userId = :userId")
+    suspend fun getLike(userId: String, postId: String): Like?
+
+    @Delete
+    suspend fun remove(like: Like)
+
+}
