7d8b624f9e6a3fc34518e46678d99dcc6b9531bc
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2021-11-03 02:34:20 +0100

    🎨 Add some util classes

---
+++ app/src/main/java/com/ebf/instant/util/signin/LoginWithGoogle.kt
@@ -0,0 +1,34 @@
+package com.ebf.instant.util.signin
+
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import com.ebf.instant.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import timber.log.Timber
+
+class LoginWithGoogle : ActivityResultContract<Unit, String?>() {
+    override fun createIntent(context: Context, input: Unit?): Intent {
+        val token = context.getString(R.string.default_web_client_id)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(token)
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(context, gso)
+        return googleSignInClient.signInIntent
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String? {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
+        return try {
+            val account = task.getResult(ApiException::class.java)
+            account.idToken
+        } catch (e: Exception) {
+            Timber.w(e, "Google sign in failed")
+            null
+        }
+    }
+}
